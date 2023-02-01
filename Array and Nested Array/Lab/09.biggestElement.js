a4e4de0b83d1699912375d32d27faa83f57845fd
Lyuben Kostov <lyubenkostov@yahoo.com>
2023-02-01 20:24:02 +0200

    Exercise

---
+++ Array and Nested Array/Lab/09.biggestElement.js
@@ -0,0 +1,27 @@
+function solve(matrix) {
+  let result = Number.NEGATIVE_INFINITY;
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col] > result) {
+        result = matrix[row][col];
+      }
+    }
+  }
+
+  return result;
+}
+
+let resultValue = solve([
+  [20, 50, 10],
+  [8, 33, 145],
+]);
+
+let resultValueTwo = solve([
+  [3, 5, 7, 12],
+  [-1, 4, 33, 2],
+  [8, 3, 0, 4],
+]);
+
+console.log(resultValue);
+console.log(resultValueTwo);
