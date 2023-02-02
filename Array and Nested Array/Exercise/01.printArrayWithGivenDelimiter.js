873424baae84c122792453e5ab9fbc3ce52095e2
Lyuben Kostov <lyubenkostov@yahoo.com>
2023-02-02 14:54:10 +0200

    Exercise

---
+++ Array and Nested Array/Exercise/01.printArrayWithGivenDelimiter.js
@@ -0,0 +1,5 @@
+function solve(array, delimitir) {
+    return array.join(delimitir)
+}
+console.log(solve(["One", "Two", "Three", "Four", "Five"], "-"));
+solve(["How about no?", "I", "will", "not", "do", "it!"], "_");
