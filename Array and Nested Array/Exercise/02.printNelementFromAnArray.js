a54a621555f83434623c92abe37892117fb7169e
Lyuben Kostov <lyubenkostov@yahoo.com>
2023-02-02 14:54:35 +0200

    Exercise

---
+++ Array and Nested Array/Exercise/02.printNelementFromAnArray.js
@@ -0,0 +1,12 @@
+function solve(array, step) {
+    // let result = [];
+    // for(let i = 0; i < array.length; i+= step){
+    //     result.push(array[i]);
+    // }
+
+    // return result;
+
+    return array.filter((_, index) => index % step == 0);
+}
+console.log(solve(["5", "20", "31", "4", "20"], 2));
+console.log(solve(["1", "2", "3", "4", "5"], 6));
