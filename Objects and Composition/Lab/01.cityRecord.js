16ad06984e2fd29bb25a1db23c95add23392ddf7
Lyuben Kostov <lyubenkostov@yahoo.com>
2023-02-02 15:02:16 +0200

    Exercise

---
+++ Objects and Composition/Lab/01.cityRecord.js
@@ -0,0 +1,18 @@
+function solve(name, population, treasury) {
+//   let myObj = {
+//     name: name,
+//     population: population,
+//     treasury: treasury,
+//   };
+
+//   return myObj;
+
+  let myObj = {
+    name,
+    population,
+    treasury
+  };
+
+  return myObj;
+}
+console.log(solve("Tortuga", 7000, 15000));
