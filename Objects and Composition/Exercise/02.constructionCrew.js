3bc53f6c2b9664eecdc34d814d68df2369e70d8f
Lyuben Kostov <lyubenkostov@yahoo.com>
2023-02-06 13:29:03 +0200

    EXe

---
+++ Objects and Composition/Exercise/02.constructionCrew.js
@@ -0,0 +1,21 @@
+function solve(worker) {
+  // if(worker.dizziness){
+  //     worker.levelOfHydrated = worker.weight * 0.1 * worker.experience;
+  //     worker.dizziness = false;
+  // }
+
+  // return worker
+
+  if (worker.dizziness){
+        return {
+        ...worker,
+        levelOfHydrated: worker.weight * 0.1 * worker.experience,
+        dizziness: false,
+        };
+    }
+
+    return {...worker};
+}
+console.log(
+  solve({ weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true })
+);
